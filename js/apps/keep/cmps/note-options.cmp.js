6e33f5d0d92a3f94049ed8de28373124fe21a60d
Amit Sharafi <amitsharafi9@gmail.com>
2022-07-01 01:16:18 +0300

    change-color feature added

--- js/apps/keep/cmps/note-options.cmp.js
+++ js/apps/keep/cmps/note-options.cmp.js
@@ -4,11 +4,21 @@ export default {
   props: ['note'],
   template: `
 			<section class="note-options">
-				<i @click="notePin(note.id)" :class="isPinned" class="fa-solid fa-thumbtack"></i>
-				<i @click="noteEdit(note.id)" :class="isEdit" class="fa-solid fa-pen-to-square"></i>
-				<i @click="noteDelete(note.id)" class="fa-solid fa-trash-can"></i>
+				<i @click="notePin(note.id)" :class="isPinned" class="fa-solid fa-thumbtack" title="Pin"></i>
+				<i @click="noteEdit(note.id)" :class="isEdit" class="fa-solid fa-pen-to-square" title="Edit"></i>
+				<i @click="noteDelete(note.id)" class="fa-solid fa-trash-can" title="Delete"></i>
+        <i class="fa-solid fa-palette" title="Background Color">
+        </i>
+        <input @input="setColor({noteId: note.id, color})" type="color" v-model="color" class="color-change"/>
+        
 			</section>
 		`,
+
+  data() {
+    return {
+      color: null,
+    }
+  },
 
   methods: {
     noteDelete(noteId) {
@@ -19,6 +29,9 @@ export default {
     },
     notePin(noteId) {
       eventBus.emit('note-pin', noteId)
+    },
+    setColor(idColorObj) {
+      eventBus.emit('color-change', idColorObj)
     },
   },
 
