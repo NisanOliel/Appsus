0c5133ce28c40729035329989c119198aec7b2e0
Amit Sharafi <amitsharafi9@gmail.com>
2022-07-02 16:13:10 +0300

    notes-add-section changes

--- js/pages/about-page.cmp.js
+++ js/pages/about-page.cmp.js
@@ -1,14 +1,12 @@
-
 export default {
-    template: `
+  template: `
  <section class="about-page">
     <h1>Nisan oliel</h1>
     <p>The one and the only one!</p>
     <img src="images/nisan-profile.jpg" alt="">
     <h1>Amit Sharfi</h1>
     <p>lorem5</p>
-    <img src="images/nisan-profile.jpg" alt="">
+    <img src="images/amit-profile.jpg" alt="">
  </section>
 `,
-
-};
+}
